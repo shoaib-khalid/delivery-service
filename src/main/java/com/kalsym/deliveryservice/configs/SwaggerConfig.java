3d0bb98ad6bc68bf2181954cc7873885c0759cdb
irasakumarvijayan <irasakumarvijayan@gmail.com>
2021-08-26 11:33:55 +0800

    version updated

--- src/main/java/com/kalsym/deliveryservice/configs/SwaggerConfig.java
+++ src/main/java/com/kalsym/deliveryservice/configs/SwaggerConfig.java
@@ -33,6 +33,6 @@ public class SwaggerConfig {
                 .description("Used to deliver item via delivery partners")
                 .termsOfServiceUrl("not added yet")
                 .license("not added yet")
-                .licenseUrl("").version("1.0").build();
+                .licenseUrl("").version("2.0.3-beta").build();
     }
 }
