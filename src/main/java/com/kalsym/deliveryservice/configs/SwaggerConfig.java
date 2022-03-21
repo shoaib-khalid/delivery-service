1cf520bb1b55154ba334c1a4c7a2b4636e421e59
Irasakumar <irasakumarvijayan@gmail.com>
2022-03-21 14:34:32 +0800

    Version 2.3.14 Db Update for get delivery vehicle type.

--- src/main/java/com/kalsym/deliveryservice/configs/SwaggerConfig.java
+++ src/main/java/com/kalsym/deliveryservice/configs/SwaggerConfig.java
@@ -46,7 +46,7 @@ public class SwaggerConfig {
                 .description("Used to deliver item via delivery partners")
                 .termsOfServiceUrl("not added yet")
                 .license("not added yet")
-                .licenseUrl("").version("2.6.13-production").build();
+                .licenseUrl("").version("2.6.14-production").build();
     }
     
     @Bean
