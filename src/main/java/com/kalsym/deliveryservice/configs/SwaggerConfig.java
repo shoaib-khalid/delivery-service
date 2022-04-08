a2fb1286f8807ca72a0fce3796b66dc96d4e5335
Irasakumar <irasakumarvijayan@gmail.com>
2022-04-08 10:06:13 +0800

    delivery-service-2.6.23

--- src/main/java/com/kalsym/deliveryservice/configs/SwaggerConfig.java
+++ src/main/java/com/kalsym/deliveryservice/configs/SwaggerConfig.java
@@ -46,7 +46,7 @@ public class SwaggerConfig {
                 .description("Used to deliver item via delivery partners")
                 .termsOfServiceUrl("not added yet")
                 .license("not added yet")
-                .licenseUrl("").version("2.6.21-production").build();
+                .licenseUrl("").version("2.6.23-production").build();
     }
     
     @Bean
