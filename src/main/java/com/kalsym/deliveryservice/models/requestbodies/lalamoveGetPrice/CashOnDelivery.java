78fe79edf10bf1ea3bcbd3cea6cc58d1720e9c47
Irasakumar <irasakumarvijayan@gmail.com>
2022-06-27 16:47:08 +0800

    Lalamove V3 upgrade

---
+++ src/main/java/com/kalsym/deliveryservice/models/requestbodies/lalamoveGetPrice/CashOnDelivery.java
@@ -0,0 +1,5 @@
+package com.kalsym.deliveryservice.models.RequestBodies.lalamoveGetPrice;
+
+public class CashOnDelivery {
+    public String amount;
+}
---
+++ src/main/java/com/kalsym/deliveryservice/models/requestbodies/lalamoveGetPrice/Item.java
@@ -0,0 +1,8 @@
+package com.kalsym.deliveryservice.models.RequestBodies.lalamoveGetPrice;
+
+public class Item {
+    public String quantity;
+    public String weight;
+    public String[] categories;
+    public String[] handlingInstructions;
+}
